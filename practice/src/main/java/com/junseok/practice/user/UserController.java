024575a3745f420ea1425afa75c87df1ca622990
junseok <junseok.oh@gentlepie.com>
2023-07-09 23:53:09 +0900

    crud

--- practice/src/main/java/com/junseok/practice/user/UserController.java
+++ practice/src/main/java/com/junseok/practice/user/UserController.java
@@ -3,10 +3,7 @@ package com.junseok.practice.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,5 +35,35 @@ public class UserController {
         User createdUser = userService.signUpUser(userRequestDto);
         UserResponseDto response = new UserResponseDto(createdUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        UserResponseDto user = userService.findUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
+        UserResponseDto updatedUser = userService.updateUser(id, userRequestDto);
+        if (updatedUser != null) {
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        boolean deleted = userService.deleteUser(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- practice/src/main/java/com/junseok/practice/user/UserService.java
+++ practice/src/main/java/com/junseok/practice/user/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +22,36 @@ public class UserService {
                 .password((userRequestDto.getPassword()))
                 .build();
         return userRepository.save(user);
+    }
+
+    public UserResponseDto findUserById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.map(UserResponseDto::new).orElse(null);
+    }
+
+    public UserResponseDto updateUser(Long id, UserRequestDto userRequestDto) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user = user.builder()
+                    .email(userRequestDto.getEmail())
+                    .password(userRequestDto.getPassword())
+                    .build();
+            User updatedUser = userRepository.save(user);
+            return new UserResponseDto(updatedUser);
+        } else {
+            return null;
+        }
+    }
+
+
+    public boolean deleteUser(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            userRepository.delete(optionalUser.get());
+            return true;
+        } else {
+            return false;
+        }
     }
 }
